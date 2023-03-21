08b6bb3571d4d59075f55593802b1c292bb1f9a8
idealy-ma <r.idealy.ma@gmail.com>
2023-03-21 07:08:08 +0000

    Front map

--- src/java/controller/PlateauController.java
+++ src/java/controller/PlateauController.java
@@ -6,46 +6,15 @@
 package controller;
 
 import dao.HibernateDao;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.logging.Level;
-import javax.servlet.http.Part;
 import model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.sql.Date;
-import java.sql.Timestamp;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import service.FilmService;
 import service.PlateauService;
 
 /**
@@ -121,5 +90,10 @@ public class PlateauController {
         pI.setPlateauId(Integer.parseInt(req.getParameter("plateauId")));
         service.insertPlateauIndispo(pI);
         return "redirect:liste_plateau";
+    }
+    
+    @RequestMapping(value = "/popup")
+    public String add_indispo(HttpServletRequest req, Model m) {
+        return "popup";
     }
 }
